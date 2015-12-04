9e70abc9d4a6cbfd28177639c6175768c9c9f734
jpanopio <jcpanopio1@gmail.com>
2015-12-04 15:37:27 -0800

    Created svg objects

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,22 @@
 // start slingin' some d3 here.
+var enemyData = [];
+for (var i = 0; i < 10; i++) {
+  enemyData.push({'index': i,'x':'x', 'y':'y'});
+}
+
+
+var enemies = d3.select('.board').selectAll('svg').data(enemyData)  
+  .enter().append('svg')
+  .style({width: 50 + 'px', height: 50 + 'px'})
+  .append('image')
+  .attr('xlink:href', 'asteroid.png')
+  .style({width: 50 + 'px', height: 50 + 'px'});
+
+
+
+
+
+
+// makeEnemies(10);
+
+// d3.select('.board').eappend(enemies);
