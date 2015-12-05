a272f0077fce174e0d0cd5bd908cefe61f35a7c5
jpanopio <jcpanopio1@gmail.com>
2015-12-04 21:04:35 -0800

    Start implementing collision detection

--- client/watchout.js
+++ client/watchout.js
@@ -6,7 +6,7 @@ var rand = function(num) {
 
 var enemyData = [];
 
-var n=10;
+var n=20;
 
 for (var i = 0; i < n; i++) {
   enemyData.push({
@@ -18,6 +18,7 @@ for (var i = 0; i < n; i++) {
 
 var height = 0.9 * window.innerHeight;
 var width = 0.9 * window.innerWidth;
+
 
 var svgElement = d3.select('.board').selectAll('svg').data([1])  
     .enter().append('svg')
@@ -31,6 +32,8 @@ var dragmove = function(d) {
   var x = d3.event.x;
   var y = d3.event.y;
   fighter.attr("transform", "translate(" + x + "," + y + ")");
+  fighterData[0]['x'] = x;
+  fighterData[0]['y'] = y;
 }
 
 
@@ -40,8 +43,6 @@ var fighter = svgElement.selectAll('circle')
     .data(fighterData, function(d) {return d.index;})
     .enter()
     .append('circle')
-    /*.attr('cx', function(d){return d.x})
-    .attr('cy', function(d){return d.y})*/
     .attr('r', '25')
     .attr('fill', 'red')
     .call(drag);
@@ -58,15 +59,34 @@ var enemies = d3.select('.board').select('svg').selectAll('image').data(enemyDat
 
 var move = function() {
   for (var i = 0; i < n; i++) {
-    var tempObj = enemyData[i];
-    tempObj['x'] = rand(width);
-    tempObj['y'] = rand(height);
+    enemyData[i]['x'] = rand(width);
+    enemyData[i]['y'] = rand(height);
   }
 
+  // IMPROVE TWEEN!
   d3.select('.board').selectAll('svg').selectAll('image').data(enemyData, function(d) {return d.index;})  
-  .transition().duration(1000)
+  .transition()
+  .tween(null, function() {
+    var fighterXIndex = fighterData[0]['x'];
+    var fighterYIndex = fighterData[0]['y'];
+
+    for(var i = 0; i < enemyData.length; i++) {
+      var moverXIndex = enemyData[i]['x'] + 25;
+      var moverYIndex = enemyData[i]['y'] + 25;
+      if (Math.sqrt(Math.pow((fighterXIndex - moverXIndex),2) + Math.pow((fighterYIndex - moverYIndex),2)) < 50) {
+        console.log('Collision!!!');
+      }
+    }
+  })
+  .duration(1000)
   .attr('x', function(d){return d.x})
   .attr('y', function(d){return d.y});
 };
 
+
+
 setInterval(move, 500);
+
+
+
+
