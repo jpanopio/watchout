4643e434ac7cbd3a5ff0415e8b0056c11a5a8716
jpanopio <jcpanopio1@gmail.com>
2015-12-04 16:25:16 -0800

    Created svg element containing enemies

--- client/watchout.js
+++ client/watchout.js
@@ -1,22 +1,25 @@
 // start slingin' some d3 here.
-var enemyData = [];
-for (var i = 0; i < 10; i++) {
-  enemyData.push({'index': i,'x':'x', 'y':'y'});
-}
+  var enemyData = [];
+  for (var i = 0; i < 10; i++) {
+    enemyData.push({'index': i,'x':10, 'y':50});
+  }
+var makeEnemies = function() {
+  var svgElement = d3.select('.board').selectAll('svg').data([1])  
+    .enter().append('svg')
+    .style({width: 700 + 'px', height: 500 + 'px'});
 
+  var enemies = d3.select('.board').select('svg').selectAll('image').data(enemyData)
+    .enter()
+    .append('image')
+    .attr('xlink:href', 'asteroid.png')
+    .style({width: 50 + 'px', height: 50 + 'px'});
+};
 
-var enemies = d3.select('.board').selectAll('svg').data(enemyData)  
-  .enter().append('svg')
-  .style({width: 50 + 'px', height: 50 + 'px'})
-  .append('image')
-  .attr('xlink:href', 'asteroid.png')
-  .style({width: 50 + 'px', height: 50 + 'px'});
-
-
-
-
-
-
-// makeEnemies(10);
+makeEnemies(10);
 
 // d3.select('.board').eappend(enemies);
+var move = function() {
+  d3.select('.board').selectAll('svg').selectAll('image').data(enemyData)  
+  .transition().duration(1000).attr('transform', 'translate(100, 500)');
+}
+
