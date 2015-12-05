39b48eee178b96dfe94fa13f186a18393530da69
jpanopio <jcpanopio1@gmail.com>
2015-12-05 15:31:00 -0800

    Improve collision detection

--- client/watchout.js
+++ client/watchout.js
@@ -1,19 +1,19 @@
 // start slingin' some d3 here.
 
 var rand = function(num) {
-  return Math.floor(Math.random()*num);
+  return Math.floor(Math.random()*(num - imageWidth));
 };
 
 var enemyData = [];
 var score = 0;
 var collisionCount = 0;
 var highScore = 0;
-var n=2;
+var n = 1;
+var imageWidth = 30;
 
-var height = 700;
-var width = 500;
-// var height = 0.9 * window.innerHeight;
-// var width = 0.9 * window.innerWidth;
+var height = 0.95 * window.innerHeight - 50;
+var width =  0.95 * window.innerWidth;
+
 for (var i = 0; i < n; i++) {
   enemyData.push({
     'index': i, 
@@ -23,13 +23,13 @@ for (var i = 0; i < n; i++) {
 }
 
 
-
 var svgElement = d3.select('.board').selectAll('svg').data([1])  
   .enter().append('svg')
   .style({width: width + 'px', height: height + 'px'});
 
 
 var fighterData = [{'index': 1, 'x': rand(width), 'y': rand(height)}];
+
 
 var dragmove = function(d) {
   var x = d3.event.x;
@@ -42,6 +42,7 @@ var dragmove = function(d) {
 
 var drag = d3.behavior.drag().on("drag", dragmove);
 
+
 var fighter = svgElement.selectAll('circle')
     .data(fighterData, function(d) {return d.index;})
     .enter()
@@ -49,6 +50,7 @@ var fighter = svgElement.selectAll('circle')
     .attr('r', '25')
     .attr('fill', 'red')
     .call(drag);
+
 
 var incrementCollision = function() {
   collisionCount++;
@@ -64,29 +66,31 @@ var incrementCollision = function() {
   } 
 }
 
+
 var throttledFunction = _.throttle(incrementCollision, 500, {trailing: false});
 
-var detectCollisions = function() {
-  var fighterXIndex = fighterData[0]['x'];//('cx');
-  var fighterYIndex = fighterData[0]['y'];//('cy');
-  var moverXIndex = d3.select('.board').select('svg').selectAll('image').attr('x');
-  
-  var moverYIndex = d3.select('.board').select('svg').selectAll('image').attr('y');
-  if (Math.sqrt(Math.pow((fighterXIndex - moverXIndex),2) + Math.pow((fighterYIndex - moverYIndex),2)) < 50) {
-    //console.log('Collision!!!', fighterXIndex, fighterYIndex, 'enemy', moverXIndex, moverYIndex);
-    throttledFunction();
-  }
-}
 
+var detectCollisions = function() {
+  var fighterXIndex = fighterData[0]['x'];
+  var fighterYIndex = fighterData[0]['y'];
+  
+  enemies[0].forEach( 
+    function(item){ 
+      moverXIndex = item['x'].animVal.value + imageWidth/2;
+      moverYIndex = item['y'].animVal.value + imageWidth/2;
+
+      if (Math.sqrt(Math.pow((fighterXIndex - moverXIndex),2) + Math.pow((fighterYIndex - moverYIndex),2)) < (25 + imageWidth/2)) {
+        throttledFunction();
+      }
+  });
+};
 
 
 var enemies = d3.select('.board').select('svg').selectAll('image').data(enemyData, function(d) {return d.index;})
     .enter()
     .append('image')
     .attr('xlink:href', 'asteroid.png')
-    .style({width: 50 + 'px', height: 50 + 'px'});
-    // .attr('x', function(d){return d.x})
-    // .attr('y', function(d){return d.y});
+    .style({width: imageWidth + 'px', height: imageWidth + 'px'});
 
 
 var move = function() {
@@ -96,23 +100,23 @@ var move = function() {
   }
 
   d3.select('.board').selectAll('svg').selectAll('image').data(enemyData, function(d) {return d.index;})  
-  .transition().duration(1000)
+  .transition().duration(3000)
   .tween(null, function() {
     return function() {
       detectCollisions();
     }
-    
   })
   .attr('x', function(d){return d.x})
   .attr('y', function(d){return d.y})
   };
 
 
-
 var incrementScore = function() {
   score++;
   d3.select('.current').text('Current score: ' + score);
 }
+
+var selectImage = d3.select('.board').select('svg').selectAll('image');
 
 setInterval(move, 1000);
 setInterval(detectCollisions, 50);
